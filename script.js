dc8ab1fa80325d320c0d90bd6e23ddb746106416
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-05-23 22:34:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your code here
+body {
+font-family: Arial, sans-serif;
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+margin: 0;
+background-color: #f5f5f5;
+}
+
+.pricing-card {
+background-color: #ffffff;
+border-radius: 10px;
+box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+padding: 2rem;
+width: 300px;
+text-align: center;
+}
+
+.pricing-card__header {
+background-color: #4CAF50;
+color: #ffffff;
+padding: 1rem;
+border-radius: 10px 10px 0 0;
+}
+
+.pricing-card__title {
+margin: 0;
+font-size: 1.5rem;
+font-weight: bold;
+}
+
+.pricing-card__price {
+font-size: 2rem;
+font-weight: bold;
+margin: 1rem 0;
+}
+
+.pricing-card__features {
+list-style-type: none;
